55632d95d56438282afba0de5d3b10fb342bfecc
Isaias Junior <icastrosi@minsait.com>
2025-06-11 15:39:10 -0300

    Conta bancária finalizada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner = true
+        Scanner sc = new Scanner(System.in);
+        //Exibir as mensagens para o nosso usuário
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = sc.nextInt();
+        System.out.print("Por gentileza, informe a agência: ");
+        String agencia = sc.next();
+        System.out.print("Para seguirmos informe seu nome: ");
+        String nomeCliente = sc.next();
+        sc.nextLine();
+        System.out.print("Informe seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.printf("\n Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+        sc.close();
     }
 }
